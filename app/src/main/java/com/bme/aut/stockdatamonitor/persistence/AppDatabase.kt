65ebde49c3f3d3ad1ed7cae269384b6537756a11
Balint Horvath <h.balint12@gmail.com>
2022-04-23 22:49:09 +0200

    e

--- app/src/main/java/com/bme/aut/stockdatamonitor/persistence/AppDatabase.kt
+++ app/src/main/java/com/bme/aut/stockdatamonitor/persistence/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import hu.bme.aut.android.stockdatamonitor.model.StockData
 
 
-@Database(entities = [StockData::class], version = 1, exportSchema = true)
+@Database(entities = [StockData::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun stockDataDao(): StockDataDao
 }
