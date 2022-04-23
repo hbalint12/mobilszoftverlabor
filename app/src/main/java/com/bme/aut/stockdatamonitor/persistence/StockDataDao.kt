9ee2ffff29927ef9d745e27535997338689d5247
Balint Horvath <h.balint12@gmail.com>
2022-04-23 23:05:03 +0200

    e

--- app/src/main/java/com/bme/aut/stockdatamonitor/persistence/StockDataDao.kt
+++ app/src/main/java/com/bme/aut/stockdatamonitor/persistence/StockDataDao.kt
@@ -1,20 +1,12 @@
 package hu.bme.aut.android.stockdatamonitor.persistence
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import hu.bme.aut.android.stockdatamonitor.model.StockData
 
 
 @Dao
 interface StockDataDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertStockDataList(stockdata: List<StockData>)
-
-    @Query("SELECT * FROM stockData WHERE name = :name")
-    suspend fun getStockData(name: String): StockData?
-
     @Query("SELECT * FROM stockData")
     suspend fun getStockDataList(): List<StockData>
 }
