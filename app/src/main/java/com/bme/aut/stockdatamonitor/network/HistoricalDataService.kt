1534584dce6823d0be4798e7314fb1d498f7737d
Balint Horvath <h.balint12@gmail.com>
2022-04-24 17:15:27 +0200

    bug fix

--- app/src/main/java/com/bme/aut/stockdatamonitor/network/HistoricalDataService.kt
+++ app/src/main/java/com/bme/aut/stockdatamonitor/network/HistoricalDataService.kt
@@ -1,9 +1,10 @@
 package hu.bme.aut.android.stockdatamonitor.network
 
 import hu.bme.aut.android.stockdatamonitor.model.StockData
+import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.GET
 
 interface HistoricalDataService {
     @GET("StockData2.json")
-    suspend fun fetchStockDataList()
+    suspend fun fetchStockDataList(): ApiResponse<List<StockData>>
 }
