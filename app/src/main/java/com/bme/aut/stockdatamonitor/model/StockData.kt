fcde19d188e7c88480eb90e6a12f8662d8a806d1
Balint Horvath <h.balint12@gmail.com>
2022-04-24 16:32:09 +0200

    update

--- app/src/main/java/com/bme/aut/stockdatamonitor/model/StockData.kt
+++ app/src/main/java/com/bme/aut/stockdatamonitor/model/StockData.kt
@@ -8,17 +8,26 @@ import javax.annotation.concurrent.Immutable
 @Entity
 @Immutable
 data class StockData(
-    @PrimaryKey val name: String,
-    val date: String,
-    val close: Double
+        @PrimaryKey(autoGenerate = true)
+        val id: Int,
+        val name: String,
+        val date: String,
+        val open: Double
+        val close: Double
+        val low: Double
+        val high: Double
 ){
 
     companion object {
 
         fun mock() = StockData(
+                id = 1,
                 name = "AAPL",
                 date = "2022-04-13",
                 close = 165.3
+                open = 164.3
+                low = 163.3
+                high = 175.3
         )
     }
 }
--- app/src/main/java/com/bme/aut/stockdatamonitor/network/HistoricalDataService.kt
+++ app/src/main/java/com/bme/aut/stockdatamonitor/network/HistoricalDataService.kt
@@ -4,6 +4,6 @@ import hu.bme.aut.android.stockdatamonitor.model.StockData
 import retrofit2.http.GET
 
 interface HistoricalDataService {
-    //@GET("StockData2.json")
+    @GET("StockData2.json")
     suspend fun fetchStockDataList()
 }
